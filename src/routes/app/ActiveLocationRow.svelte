a5990b97e07fa827a42b265b342353433ddc170f
Playwo <eliaswolf1609@gmail.com>
2023-09-19 21:53:01 +0200

    Add connect button

--- src/routes/app/ActiveLocationRow.svelte
+++ src/routes/app/ActiveLocationRow.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { NATIVE_API } from '$lib/native-api/native-api';
 	import { type Wallet, WALLET } from '$lib/wallet/wallet';
 
 	export let dseq: number;
@@ -13,6 +14,10 @@
 
 	async function triggerCloseDeployment(dseq: number) {
 		await wallet.closeDeployment(dseq);
+	}
+
+	async function triggerConnectVPN(host: string) {
+		await NATIVE_API.connectVPN(host, 'admin', 'notreallyasecretpassword');
 	}
 </script>
 
@@ -37,21 +42,34 @@
 			{Math.round((new Date().getTime() - timestamp.getTime()) / 60000)} mins
 		{/await}
 	</td>
-	<td>
-		{#if gseq != null && oseq != null}
-			{#await wallet.getProviderLeaseStatus(dseq, gseq, oseq, `${provider}`)}
-				...
-			{:then leaseStatus}
+
+	{#if gseq != null && oseq != null}
+		{#await wallet.getProviderLeaseStatus(dseq, gseq, oseq, `${provider}`)}
+			<td> ... </td>
+			<td>
+				<button on:click={() => triggerCloseDeployment(dseq)}>Close</button>
+			</td>
+		{:then leaseStatus}
+			<td>
 				<p>
 					{`${leaseStatus.forwardedPorts[0].host}:${leaseStatus.forwardedPorts[0].externalPort}`}
 				</p>
-			{/await}
-		{:else}
-			<p>Not Connected</p>
-		{/if}
-	</td>
-	<td>
-		<button>Connect</button>
-		<button on:click={() => triggerCloseDeployment(dseq)}>Close</button>
-	</td>
+			</td>
+
+			<td>
+				<button
+					on:click={() =>
+						triggerConnectVPN(
+							`${leaseStatus.forwardedPorts[0].host}:${leaseStatus.forwardedPorts[0].externalPort}`
+						)}>Connect</button
+				>
+				<button on:click={() => triggerCloseDeployment(dseq)}>Close</button>
+			</td>
+		{/await}
+	{:else}
+		<td><p>Not Connected</p></td>
+		<td>
+			<button on:click={() => triggerCloseDeployment(dseq)}>Close</button>
+		</td>
+	{/if}
 </tr>
