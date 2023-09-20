705dbab784b2c2fa4378c5b8c1cfa562963005e3
leonlolly <leonlolly60952@gmail.com>
2023-09-20 15:47:27 +0200

    add dev native api

--- src/lib/native-api/native-api.ts
+++ src/lib/native-api/native-api.ts
@@ -15,15 +15,32 @@ export interface NativeAPIs {
 	): Promise<T>;
 	vpnClientStatus(): Promise<VPNClientStatus>;
 	connectVPN(host: string, username: string, password: string): Promise<void>;
+	disconnectVPN(): Promise<void>;
 }
 
 export var NATIVE_API: NativeAPIs = null!;
 
-export var initializeNativeAPI: () => Promise<boolean> = () => Promise.reject('Not Implemented');
+export var initializeNativeAPI: () => Promise<boolean> = () =>
+	Promise.reject('Not Implemented');
 
-export function setNativeAPIInitializer(getNativeAPI: () => Promise<NativeAPIs>) {
+export function setNativeAPIInitializer(
+	getNativeAPI: () => Promise<NativeAPIs>
+) {
 	initializeNativeAPI = async () => {
 		NATIVE_API = await getNativeAPI();
 		return true;
 	};
 }
+
+setNativeAPIInitializer(async () => {
+	return {
+		loadFile: (path) => Promise.resolve(localStorage.getItem(path)),
+		saveFile: (path, content) =>
+			Promise.resolve(localStorage.setItem(path, content)),
+		mtlsFetch: (method, url, body, csr, privateKey) =>
+			Promise.resolve(5 as any),
+		vpnClientStatus: () => Promise.resolve('Running'),
+		connectVPN: (host, username, password) => Promise.resolve(),
+		disconnectVPN: () => Promise.resolve()
+	} satisfies NativeAPIs;
+});
