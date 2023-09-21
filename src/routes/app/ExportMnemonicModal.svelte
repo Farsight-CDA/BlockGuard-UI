980cbb1bbf62405587b12ec924d64a85be6d4eb0
leonlolly <leonlolly60952@gmail.com>
2023-09-21 18:37:42 +0200

    add mnemonic export

--- src/routes/app/ExportMnemonicModal.svelte
+++ src/routes/app/ExportMnemonicModal.svelte
@@ -24,8 +24,23 @@
 	>
 		<h2 class="font-bold text-lg">Export your Mnemonics</h2>
 
-		<button on:click={copyToClipboard} class="bg-green-600 p-3 rounded-md"
-			>{mnemonics}</button
-		>
-	</div>
-</Modal>
+		{#if mnemonics == null}
+			<div class="grid grid-cols-4 gap-3 p-4 rounded-xl bg-slate-800">
+				<p>No mnemonics this is weard</p>
+			</div>
+		{:else}
+			<button
+				on:click={copyToClipboard}
+				class="grid grid-cols-4 gap-3 p-4 rounded-xl bg-slate-800"
+			>
+				{#each mnemonics.split(' ') as word, i}
+					<p>{word}</p>
+				{/each}
+			</button>
+			<button
+				class="gap-3 p-4 rounded-xl bg-slate-800 transform"
+				on:click={copyToClipboard}>Copy</button
+			>
+		{/if}
+	</div></Modal
+>
