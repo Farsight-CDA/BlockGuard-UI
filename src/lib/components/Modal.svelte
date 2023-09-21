3de5cb3591531f51b10120c0d3a6c3b3cc24aa01
leonlolly <leonlolly60952@gmail.com>
2023-09-21 17:17:43 +0200

    add modal componend and implementet FundwalletModal

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let dialogElement: HTMLDialogElement;
+	var isOpen: boolean = false;
+
+	export const open = async function open() {
+		isOpen = true;
+		dialogElement.showModal();
+	};
+
+	function close() {
+		dialogElement.close();
+		isOpen = false;
+	}
+	function dialogClickHandler(e: MouseEvent) {
+		if (e.target === dialogElement) {
+			close();
+		}
+	}
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialogElement}
+	class="p-4 shadow-2xl rounded-lg bg-slate-900 backdrop:backdrop-blur-sm"
+	on:click={dialogClickHandler}
+	on:keydown={() => {}}
+>
+	{#if isOpen}
+		<slot />
+	{/if}
+</dialog>
--- src/routes/app/advanced/FundWalletModal.svelte
+++ src/routes/app/advanced/FundWalletModal.svelte
@@ -1,46 +1,23 @@
 <script lang="ts">
+	import Modal from '$lib/components/Modal.svelte';
 	import { useRequiredWallet } from '$lib/wallet/wallet';
 	import { scale } from 'svelte/transition';
 
-	let dialogElement: HTMLDialogElement;
-	var isOpen: boolean = false;
-
 	var wallet = useRequiredWallet();
 
-	export const open = async function open() {
-		isOpen = true;
-		dialogElement.showModal();
-	};
-
-	function close() {
-		dialogElement.close();
-		isOpen = false;
-	}
-	function dialogClickHandler(e: MouseEvent) {
-		if (e.target === dialogElement) {
-			close();
-		}
-	}
+	export let open: () => Promise<void>;
 </script>
 
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<dialog
-	bind:this={dialogElement}
-	class="p-4 shadow-2xl rounded-lg bg-slate-900 backdrop:backdrop-blur-sm"
-	on:click={dialogClickHandler}
-	on:keydown={() => {}}
->
-	{#if isOpen}
-		<div
-			class="flex flex-col items-center gap-4"
-			transition:scale={{ duration: 200, delay: 0 }}
-			on:introstart
-			on:outroend
-		>
-			<h2 class="font-bold text-lg">Fund your Wallet</h2>
+<Modal bind:open
+	><div
+		class="flex flex-col items-center gap-4"
+		transition:scale={{ duration: 200, delay: 0 }}
+		on:introstart
+		on:outroend
+	>
+		<h2 class="font-bold text-lg">Fund your Wallet</h2>
 
-			<p>Send at least 5.5 AKT to</p>
-			<code class="p-2 bg-slate-600 rounded-md">{$wallet.getAddress()}</code>
-		</div>
-	{/if}
-</dialog>
+		<p>Send at least 5.5 AKT to</p>
+		<code class="p-2 bg-slate-600 rounded-md">{$wallet.getAddress()}</code>
+	</div></Modal
+>
