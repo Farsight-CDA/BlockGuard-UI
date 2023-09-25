304fb78537e2076ed8e11615810925e13bb0c8dd
leonlolly <leonlolly60952@gmail.com>
2023-09-25 13:27:00 +0200

    add glow and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
 		extend: {
 			spacing: {
 				90: '22rem'
+			},
+			dropShadow: {
+				glow: [
+					'0 0px 20px rgba(255,255, 255, 0.35)',
+					'0 0px 65px rgba(255, 255,255, 0.2)'
+				],
+				// Shadows for red
+				'glow-red-100': [
+					'0 0px 20px rgba(255, 0, 0, 0.35)',
+					'0 0px 65px rgba(255, 0, 0, 0.2)'
+				],
+
+				// Shadows for green
+				'glow-green-400': [
+					'0 0px 20px rgba(74, 222, 128, 0.35)',
+					'0 0px 65px rgba(74, 222, 128, 0.2)'
+				],
+				// Shadows for blue
+				'glow-blue-400': [
+					'0 0px 20px rgba(96, 165, 250, 0.35)',
+					'0 0px 65px rgba(96, 165, 250, 0.2)'
+				]
 			}
+		},
+		fontFamily: {
+			armata: ['Armata', 'sans']
 		}
 	},
 	plugins: []
