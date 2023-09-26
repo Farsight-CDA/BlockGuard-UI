a98bf7cca02d644ade5cabe80b154865675282ce
leonlolly <leonlolly60952@gmail.com>
2023-09-26 14:15:58 +0200

    change glow + naming

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
 			},
 			dropShadow: {
 				glow: [
-					'0 0px 20px rgba(255,255, 255, 0.2)',
+					'0 0px 20px rgba(255,255, 255, 0.1)',
 					'0 0px 65px rgba(255, 255,255, 0.2)'
 				],
 				// Shadows for red
