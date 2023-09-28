b3221918d65f275927eb6a48f39ff3f4526409f4
leonlolly <leonlolly60952@gmail.com>
2023-09-28 14:44:12 +0200

    change tooltip orientation

--- src/lib/components/ToolTipFromAction.svelte
+++ src/lib/components/ToolTipFromAction.svelte
@@ -7,8 +7,8 @@
 <div
 	class="border border-white bg-neutral-700 px-3 py-1 absolute z-50 rounded-md shadow-md"
 	style="
-		top: {y + 5}px;
-		left: {x + 5}px;"
+		top: {y - 35}px;
+		left: {x - 5}px;"
 >
 	{title}
 </div>
