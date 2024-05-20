9f22c43856b306e112a38c01cb79ea9eeedf573b
Playwo <eliaswolf1609@gmail.com>
2024-05-20 11:02:30 +0200

    feat: Desktop Native API

--- src/lib/native-api/native-api.ts
+++ src/lib/native-api/native-api.ts
@@ -1,4 +1,11 @@
 import type { VPNConnectionStatus } from '$lib/vpn/types';
+import {
+	ConnectVPN,
+	DisconnectVPN,
+	GetConnectionStatus,
+	MTLSFetch,
+	SoftEtherStatus
+} from '$lib/wailsjs/go/main/App';
 import type { HttpMethod } from '@sveltejs/kit';
 
 export type VPNClientStatus = 'Offline' | 'Running';
@@ -57,4 +64,49 @@ setNativeAPIInitializer(async () => {
 		pasteFromClipboard: () =>
 			Promise.resolve(window.navigator.clipboard.readText())
 	} satisfies NativeAPIs;
+});
+
+setNativeAPIInitializer(async () => {
+	if (!Object.hasOwn(window, 'go')) {
+		window.location.href = '/';
+		throw Error('Missing window.go');
+	}
+
+	return Promise.resolve({
+		loadFile: (path) => Promise.resolve(localStorage.getItem(path)),
+		saveFile: (path, content) =>
+			Promise.resolve(localStorage.setItem(path, content)),
+		clearFile: (path) => Promise.resolve(localStorage.removeItem(path)),
+		mtlsFetch: async <T>(
+			method: HttpMethod,
+			url: string,
+			body: string,
+			csr: string,
+			privateKey: string
+		) => {
+			const res = await MTLSFetch(method, url, body, csr, privateKey);
+
+			if (!res.success) {
+				console.log(res);
+				throw Error(`MTLS Fetch failed: StatusCode ${res.statusCode}`);
+			}
+
+			try {
+				return JSON.parse(res.body) as T;
+			} catch (error) {
+				return res as T; //T better be string
+			}
+		},
+		getVPNClientStatus: async () =>
+			(await SoftEtherStatus()) as VPNClientStatus,
+		connectVPN: async (host, username, password) =>
+			await ConnectVPN(host, username, password),
+		disconnectVPN: async () => await DisconnectVPN(),
+		getConnectionStatus: async () =>
+			(await GetConnectionStatus()) as VPNConnectionStatus,
+		copyToClipboard: async (string) =>
+			Promise.resolve(await window.navigator.clipboard.writeText(string)),
+		pasteFromClipboard: () =>
+			Promise.resolve(window.navigator.clipboard.readText())
+	} satisfies NativeAPIs);
 });
